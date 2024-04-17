31a6334fbf52b033707b50b3ceb233fe29c37bd5
Izam Ghali <izamghali18@gmail.com>
2024-04-17 16:20:14 +0700

    TYPE & COMPONENT: created IButton type with svg, className, and OnClickFunc as optional property. Applied Button component on Drawer, & mapped testTopics on drawer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,15 @@
 import React from "react"
 
-export default function Button({ text, svg, className } : { text: string, svg: any, className: string }) {
+interface IButton {
+    text: string,
+    svg?: any,
+    className?: string
+    OnClickFunc?: any
+}
+
+export default function Button({ text, svg, className, OnClickFunc } : IButton) {
     return (
-        <button className={`btn ${className} `}>
+        <button onClick={OnClickFunc} className={`btn ${className} `}>
             { svg }
             { text }
         </button>
