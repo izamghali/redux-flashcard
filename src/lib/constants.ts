02ab28ef441e49ca6056a4cec4779b8609646cdc
Izam Ghali <izamghali18@gmail.com>
2024-08-04 23:08:02 +0700

    build: initial commit. Installed shadcn & some components. Set media screen on tailwind.config

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export enum CardType {
+    
+}
+
+/*
+    @ translate         : string | string
+    @ gapped sentence
+    @ antonym           : string | string
+    @ synonym           : string | string
+    @ defenition        : string | string
+    @ picture           : string | string
+*/
